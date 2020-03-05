b44697629fc457dba3fe91c46d12197f75834076
St.Anton <struichkov.anton@gmail.com>
2020-03-06 02:29:04 +0300

    Добавляет изменение адреса в форме при перемещении главного пина

--- js/main-pin.js
+++ js/main-pin.js
@@ -6,6 +6,10 @@
   var MAP_PIN_MAX_X = 1200;
   var PIN_MAIN_WIDTH = 62; // Ширина главной метки
   var PIN_MAIN_SHIFT_X = PIN_MAIN_WIDTH / 2; // Смещение круглого главного пина по оси X до центра метки
+  var RADIX = 10;
+  var PIN_MAIN_HEIGHT = 62; // Высота главной метки
+  var PIN_MAIN_POINT_SHIFT_Y = 20; // (высота острия метки) Смещение по оси Y до точки острого конца метки
+
   var mainPinMouseDownHandler = document.querySelector('.map__pin--main');
   mainPinMouseDownHandler.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -27,37 +31,51 @@
       };
       // Функция нахождения координаты по оси Y
       var coordsY = function () {
-        var CoordY = 0;
+        var coordY = 0;
         if (mainPinMouseDownHandler.offsetTop - shift.y > MAP_PIN_MAX_Y) {
-          CoordY = MAP_PIN_MAX_Y;
+          coordY = MAP_PIN_MAX_Y;
         } else if (
           mainPinMouseDownHandler.offsetTop - shift.y <
           MAP_PIN_MIN_Y
         ) {
-          CoordY = MAP_PIN_MIN_Y;
+          coordY = MAP_PIN_MIN_Y;
         } else {
-          CoordY = mainPinMouseDownHandler.offsetTop - shift.y;
+          coordY = mainPinMouseDownHandler.offsetTop - shift.y;
         }
-        return CoordY;
+        return coordY;
       };
       // Функция нахождения координаты по оси X
       var coordsX = function () {
-        var CoordX = 0;
-        if (mainPinMouseDownHandler.offsetLeft - shift.x > MAP_PIN_MAX_X - PIN_MAIN_SHIFT_X) {
-          CoordX = MAP_PIN_MAX_X - PIN_MAIN_SHIFT_X;
+        var coordX = 0;
+        if (
+          mainPinMouseDownHandler.offsetLeft - shift.x >
+          MAP_PIN_MAX_X - PIN_MAIN_SHIFT_X
+        ) {
+          coordX = MAP_PIN_MAX_X - PIN_MAIN_SHIFT_X;
         } else if (
           mainPinMouseDownHandler.offsetLeft - shift.x <
           MAP_PIN_MIN_X - PIN_MAIN_SHIFT_X
         ) {
-          CoordX = MAP_PIN_MIN_X - PIN_MAIN_SHIFT_X;
+          coordX = MAP_PIN_MIN_X - PIN_MAIN_SHIFT_X;
         } else {
-          CoordX = mainPinMouseDownHandler.offsetLeft - shift.x;
+          coordX = mainPinMouseDownHandler.offsetLeft - shift.x;
         }
-        return CoordX;
+        return coordX;
       };
-
       mainPinMouseDownHandler.style.top = coordsY() + 'px';
       mainPinMouseDownHandler.style.left = coordsX() + 'px';
+      // Функция указания адреса главной метки в активном состоянии (острая часть пина)
+      var coordinateMainPinActive = function () {
+        var pinMainX = parseInt(mainPinMouseDownHandler.style.left, RADIX) + PIN_MAIN_SHIFT_X;
+        var pinMainY =
+          parseInt(mainPinMouseDownHandler.style.top, RADIX) +
+          PIN_MAIN_HEIGHT +
+          PIN_MAIN_POINT_SHIFT_Y;
+        return pinMainX + ', ' + pinMainY;
+      };
+      // Заполнение адрессной строки по умолчанию (в неактивном состоянии)
+      var addressPin = document.querySelector('#address');
+      addressPin.value = coordinateMainPinActive();
     };
     var mainPinMouseUpHandler = function (upEvt) {
       upEvt.preventDefault();
